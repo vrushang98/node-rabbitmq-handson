0e76f46ba26c801fbbf19eacd555f5f96843048a
= <vrushang.vaishnav@tatvasoft.com>
2022-06-07 10:14:38 +0530

    session-service-added

---
+++ session-service/index.js
@@ -0,0 +1,29 @@
+const { connect } = require("amqplib");
+const express = require("express");
+const app = express();
+const amqp = require("amqplib");
+
+let channel, connection;
+connectServer();
+async function connectServer() {
+  try {
+    const amqpServer = "amqp://localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+
+    await channel.assertQueue("rabbit");
+
+    channel.consume("rabbit", (data) => {
+      console.log(`Received ${Buffer.from(data.content)}`);
+      channel.ack(data);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+app.get("/send", (req, res) => {});
+
+app.listen(5001, () => {
+  console.log(`Server at 5001`);
+});
